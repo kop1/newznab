07ca023df4fd19c338c7e94c83cf6c538c425dc4
torhenning <torhenning@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-20 10:34:36 +0000

    Fixed newline bug in newznab_update.sh
    git-svn-id: svn://svn.newznab.com/nn/trunk@136 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/cron_update/newznab_update.sh
+++ misc/cron_update/newznab_update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# if you move the cronned files to a different place you
-# may need to tweak the path in /path/to/misc/cron_update/setpath to point to root of newznab
-/path/to/php /path/to/misc/cron_update/update_binaries.php
-/path/to/php /path/to/misc/cron_update/update_releases.php
+php update_binaries.php
+php update_releases.php
