88172f2b7aefcaa69deccca2eb6fa09e70702c8b
torhenning <torhenning@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-20 15:33:37 +0000

    Added step one of the installation
    git-svn-id: svn://svn.newznab.com/nn/trunk@140 a8b8df2f-1e19-4095-b43c-2e6d618ca476

---
+++ www/install/index.php
@@ -0,0 +1,16 @@
+<?php include('header.html'); ?>
+		<h1>Welcome</h1>
+                <p>Welcome to Newznab. Before getting started, we need to make shure that your server meet the minimum requirements for running Newznab. Also prepare the following information:</p>
+                <ol>
+                        <li>Your database information</li>
+                        <li>Your news server information</li>
+                        <li>SSH & root abilities on your server (if you need to install som packages)</li>
+                </ol>
+                <p>
+                <strong><div style="color: red">WARNING: </div> This software is not and will not be made for usage on shared hosting. You should only use this on a server where YOU have the required privileges and knowledge to solve any challenges that might appear.</strong></p>
+
+		<div align="center">
+			<form action="checkit.php"><input type="submit" value="Go to step one: Pre flight check" /></form>		
+		</div>
+
+<?php include('footer.html'); ?>
