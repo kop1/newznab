62f58c23c76c17a579983344405ff8d97675a9e4
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-21 21:42:41 +0000

    updated admin/group-edit to use WWW_DIR
    git-svn-id: svn://svn.newznab.com/nn/trunk@147 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/admin/group-edit.php
+++ www/admin/group-edit.php
@@ -1,7 +1,8 @@
 <?php
 
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/adminpage.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/groups.php");
+require_once("config.php");
+require_once(WWW_DIR."/lib/adminpage.php");
+require_once(WWW_DIR."/lib/groups.php");
 
 $page = new AdminPage();
 $groups = new Groups();
