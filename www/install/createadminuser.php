cf16964b536eaab681e70e0b50bf28e8e6f30843
torhenning <torhenning@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-22 18:50:41 +0000

    Added support for creating a user in the installer
    git-svn-id: svn://svn.newznab.com/nn/trunk@165 a8b8df2f-1e19-4095-b43c-2e6d618ca476

---
+++ www/install/createadminuser.php
@@ -0,0 +1,67 @@
+<?php 
+
+require("../lib/framework/db.php");
+require("../lib/users.php");
+
+//Try to save config and import data
+if($_GET['do'] == 'run') {
+
+	$error = '';
+	$user = trim($_POST['user']);
+	$pass = trim($_POST['pass']);
+	$mail = trim($_POST['mail']);
+
+	if(strlen($user) == 0) {
+		$error = "Invalid username";
+	} else if(strlen($pass) == 0) {
+		$error = "No password set";
+	} else if(strlen($mail) == 0) {
+		$error = "No Email set";
+	}
+
+	if(strlen($error) == 0) {
+		$db = new DB;
+		$usrObject = new Users();
+		$test = $usrObject->add($user, $pass, $mail, 2, '', '', '');
+		if(is_numeric($test)) {
+			header("location: finish.php");
+			exit();
+		}
+	}
+
+}
+
+include('header.html');
+?>
+	<h1>Admin user</h1>
+	<p>You must setup a admin user. Please provide the following information</p>
+	<form action="createadminuser.php?do=run" method="post">
+		<table>
+			<tr>
+				<th>Username:</th>
+				<td><input type="text" name="user" value="<?=$user?>" /></td>
+			</tr>
+			<tr>
+				<th>Password:</th>
+				<td><input type="text" name="pass" value="<?=$pass?>" /></td>
+			</tr>
+                        <tr>
+                                <th>Email:</th>
+                                <td><input type="text" name="mail" value="<?=$mail?>" /></td>
+                        </tr>
+			<tr> 
+				<td colspan="2">
+					<div align="center">
+						<input type="submit" value="Step six: Clean up" />
+					</div>
+				</td>
+			</tr>
+		</table>
+	<?php
+		if(strlen($error) > 0) {
+			echo '<div class="error">'.$error."</div>";
+		}
+	?>
+	</form>
+
+<?php include('footer.html'); ?>
