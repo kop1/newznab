b7a9573a8f08647f7f8e6eb661d4e49aeb4f6be4
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-23 22:17:58 +0000

    updated to use WWW_DIR
    git-svn-id: svn://svn.newznab.com/nn/trunk@182 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/getnzb.php
+++ www/getnzb.php
@@ -1,8 +1,10 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/page.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/nzb.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/releases.php");
-require_once($_SERVER['DOCUMENT_ROOT']."/lib/users.php");
+
+require_once("config.php");
+require_once(WWW_DIR."/lib/page.php");
+require_once(WWW_DIR."/lib/nzb.php");
+require_once(WWW_DIR."/lib/users.php");
+require_once(WWW_DIR."/lib/releases.php");
 
 $page = new Page;
 $users = new Users;
