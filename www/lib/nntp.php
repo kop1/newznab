df8a59f8229f45a166f87900a629af0da684b940
gabypmow@gmail.com <gabypmow@gmail.com@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-05-25 22:58:39 +0000

    ret2 not set for non-auth
    git-svn-id: svn://svn.newznab.com/nn/trunk@211 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/nntp.php
+++ www/lib/nntp.php
@@ -9,12 +9,19 @@ class Nntp extends Net_NNTP_Client
 	function doConnect() 
 	{
 		$ret = $this->connect(NNTP_SERVER);
-		if(!defined(NNTP_USERNAME) && NNTP_USERNAME!="" )
-			$ret2 = $this->authenticate(NNTP_USERNAME, NNTP_PASSWORD);
-		if(PEAR::isError($ret) || PEAR::isError($ret2)) 
+		if(PEAR::isError($ret))
 		{
-			echo "Cannot connect to server - ".NNTP_SERVER." - ".NNTP_USERNAME." ($ret $ret2)";
+			echo "Cannot connect to server ".NNTP_SERVER." $ret";
 			die();
+		}
+		if(!defined(NNTP_USERNAME) && NNTP_USERNAME!="" )
+		{
+			$ret2 = $this->authenticate(NNTP_USERNAME, NNTP_PASSWORD);
+			if(PEAR::isError($ret) || PEAR::isError($ret2)) 
+			{
+				echo "Cannot authenticate to server ".NNTP_SERVER." - ".NNTP_USERNAME." ($ret $ret2)";
+				die();
+			}
 		}
 	}
 	
