e8355949bc5c9f62eaba7e0305bfdb873b924809
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-06-14 20:50:37 +0000

    add releases to initd script
    git-svn-id: svn://svn.newznab.com/nn/trunk@360 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/cron_update/newznab.sh
+++ misc/cron_update/newznab.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
-# do not forget to change NEWZNAB variables
+# do not forget to change NEWZNAB.. vars =)
 
 set -e
 
 export NEWZNAB_PATH="/usr/local/www/newznab/misc/cron_update"
 export NEWZNAB_BINUP="update_binaries.php"
+export NEWZNAB_RELUP="update_release.php"
+export NEWZNAB_SLEEP_TIME="600" # in seconds . 10sec is good for 100s of groups. 600sec might be a good start for fewer.
+export NEWZNAB_PID_PATH="/var/run/" # don't forget the trailing slash . need r/w on it
+
 
 export PATH="${PATH}:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin"
-PIDFILE="/var/run/newznab_binup.pid"
+PIDFILE="newznab_binup.pid"
 
 case "$1" in
   start)
         echo -n "Starting Newznab binaries update"
         cd ${NEWZNAB_PATH}
-        (while (true);do cd ${NEWZNAB_PATH} && php ${NEWZNAB_BINUP} 2>&1 > /dev/null ; sleep 600 ;done) & 
-        PID=`echo $!` 
-        echo $PID > $PIDFILE
+        (while (true);do cd ${NEWZNAB_PATH} && php ${NEWZNAB_BINUP} && php ${NEWZNAB_RELUP} ; sleep ${SLEEP_TIME} ;done) &
+        PID=`echo $!`
+        echo $PID > ${NEWZNAB_PID_PATH}${PIDFILE}
         ;;
   stop)
         echo -n "Stopping Newznab binaries update"
-        kill -9 `cat /var/run/newznab_binup.pid`
+        kill -9 `cat ${NEWZNAB_PID_PATH}${PIDFILE}`
         ;;
 
   *)
         echo "Usage: $0 [start|stop]"
         exit 1
 esac
-exit 0
