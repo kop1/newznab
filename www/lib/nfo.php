5b8fd3ef6eb44f8f526def61a44e23b9a18e88ae
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-06-17 23:20:03 +0000

    split nfo downloading/parsing into its own class. updated imdb id check to recognize Title?xxxxxxx format
    git-svn-id: svn://svn.newznab.com/nn/trunk@381 a8b8df2f-1e19-4095-b43c-2e6d618ca476

---
+++ www/lib/nfo.php
@@ -0,0 +1,257 @@
+<?php
+require_once("config.php");
+require_once(WWW_DIR."/lib/framework/db.php");
+require_once(WWW_DIR."/lib/nzb.php");
+require_once(WWW_DIR."/lib/nntp.php");
+
+class Nfo 
+{
+	function Nfo() 
+	{
+		//TODO
+	}
+	
+	/**
+     * determineReleaseNfo()
+     *
+     * @param array $nzbdata  array of nzb binary data provided by $nzb->getNZBforReleaseId($relid);
+     * @return array
+     */
+	public function determineReleaseNfo($nzbdata)
+	{
+		$nfos = array();
+		foreach ($nzbdata as $bin) {
+			if (preg_match('/.*\.nfo[ "\)\]\-]/i', $bin['binary']['name'])) {
+				$nfos[$bin['binary']['name']] = $bin;
+			}
+		}
+		ksort($nfos);
+		return (is_array($nfos) && !empty($nfos)) ? array_shift($nfos) : false;
+	}
+	
+	public function addReleaseNfo($relid, $binid)
+	{
+		$db = new DB();
+		return $db->queryInsert(sprintf("INSERT INTO releasenfo (releaseID, binaryID) VALUES (%d, %d)", $relid, $binid));		
+	}
+	
+	public function deleteReleaseNfo($relid)
+	{
+		$db = new DB();
+		return $db->query(sprintf("delete from releasenfo where releaseID = %d", $relid));		
+	}
+	
+	public function processNfoFiles($echooutput=false, $processImdb = true)
+	{
+		$ret = 0;
+		$db = new DB();
+		$nzb = new Nzb();
+		$nntp = new Nntp();
+	
+		$res = $db->queryDirect(sprintf("SELECT * FROM releasenfo WHERE nfo IS NULL AND attempts < 5"));
+
+		if ($res) 
+		{
+			$nntp->doConnect();
+			while ($arr = mysql_fetch_array($res, MYSQL_BOTH)) 
+			{
+				//if ($ret > 0) { continue; } //only process one nfo per run for testing
+				$binaryToFetch = $nzb->getNZB(array($arr['binaryID']));
+				$fetchedBinary = $nntp->getBinary($binaryToFetch[0]);
+				if ($fetchedBinary !== false) 
+				{
+					//insert nfo into database
+					$db->query(sprintf("UPDATE releasenfo SET nfo = compress(%s) WHERE ID = %d", $db->escapeString($fetchedBinary), $arr["ID"]));
+					$ret++;
+					
+					//scan for imdb info if set
+					if ($processImdb)
+					{
+						$imdbId = $this->parseImdb($fetchedBinary);
+						if ($imdbId !== false) 
+						{
+							//update release with imdb id
+							$db->query(sprintf("UPDATE releases SET imdbID = %s WHERE ID = %d", $db->escapeString($imdbId), $arr["releaseID"]));
+							
+							//check for existing movie entry
+							$movCheck = $db->queryOneRow(sprintf("SELECT ID FROM movieinfo where imdbID = %d", $imdbId));
+							if ($movCheck === false)
+							{
+								if ($echooutput)
+									echo "fetching imdb info from tmdb - ".$imdbId."\n";
+								
+								//check themoviedb for imdb info
+								$imdb = $this->fetchTvdbProperties($imdbId);
+								if ($imdb === false) {
+									if ($echooutput)
+										echo "not found in tmdb... trying from imdb - ".$imdbId."\n";
+									
+									//check imdb for movie info
+									$imdb = $this->fetchImdbProperties($imdbId);
+									if ($imdb === false) {
+										if ($echooutput)
+											echo "unable to get movie info from imdb - ".$imdbId."\n";
+									}
+
+								}
+																
+								if ($imdb !== false) {
+									$imdb['tmdb_id'] = (!isset($imdb['tmdb_id']) || $imdb['tmdb_id'] == '') ? "NULL" : $imdb['tmdb_id'];
+									if (isset($imdb['cover']) && $imdb['cover'] != '') {
+										$cover = $this->fetchReleaseImage($imdb['cover']);
+										if ($cover !== false) {
+											$imdb['cover'] = $cover;
+										} else {
+											if ($echooutput)
+												echo "cover download failed - ".$imdb['cover']."\n";
+										}
+									}
+									if (isset($imdb['backdrop']) && $imdb['backdrop'] != '') {
+										$backdrop = $this->fetchReleaseImage($imdb['backdrop']);
+										if ($backdrop !== false) {
+											$imdb['backdrop'] = $backdrop;
+										} else {
+											if ($echooutput)
+												echo "backdrop download failed - ".$imdb['backdrop']."\n";
+										}
+									}
+								
+									$query = sprintf("INSERT INTO movieinfo (imdbID, tmdbID, title, rating, plot, year, genre, cover, backdrop, createddate) VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s, NOW())", $imdb['imdb_id'], $imdb['tmdb_id'], $db->escapeString($imdb['title']), $db->escapeString($imdb['rating']), $db->escapeString($imdb['plot']), $db->escapeString($imdb['year']), $db->escapeString($imdb['genre']), $db->escapeString($imdb['cover']), $db->escapeString($imdb['backdrop']));
+									$movieId = $db->queryInsert($query);
+								
+									if ($echooutput && $movieId)
+										echo "added movie: ".$imdb['title']." (".$imdb['year'].") - ".$imdb['imdb_id']."\n";
+									
+								} //no data fetched
+							} //no local version of movie
+						} //no imdb id found
+					} //no imdb processing
+				} 
+				else 
+				{
+					if ($echooutput)
+						echo "nfo download failed - release ".$arr['releaseID']." on attempt ".($arr["attempts"]++)."\n";
+						
+					//nfo download failed, increment attempts
+					$db->query(sprintf("UPDATE releasenfo SET attempts = attempts+1 WHERE ID = %d", $arr["ID"]));
+				}
+			}
+			$nntp->doQuit();
+		}
+		
+		if ($echooutput)
+			echo $ret." nfo files processed\n";
+		
+		return $ret;
+	}
+	
+	private function fetchReleaseImage($imgUrl)
+	{		
+		$img = @file_get_contents($imgUrl);
+		if ($img !== false) {
+			$im = @imagecreatefromstring($img);
+			if($im !== false) {
+				return $img;
+			}
+		}
+		return false;	
+	}
+	
+	private function parseImdb($str) {
+		preg_match('/imdb.*?(tt|Title\?)(\d{7})/i', $str, $matches);
+		if (isset($matches[2]) && !empty($matches[2])) {
+			return trim($matches[2]);
+		}
+		return false;
+	}
+	
+	private function fetchTvdbProperties($imdbId)
+	{
+		require_once(WWW_DIR."/lib/TMDb.php");
+		$tmdb = new TMDb('9a4e16adddcd1e86da19bcaf5ff3c2a3');
+		$lookupId = 'tt'.$imdbId;
+		$movie = array_shift(json_decode($tmdb->getMovie($lookupId, TMDb::IMDB)));
+		if ($movie == 'Nothing found.') { return false; }
+		$ret = array();
+		$ret['title'] = $movie->name;
+		$ret['tmdb_id'] = $movie->id;
+		$ret['imdb_id'] = $imdbId;
+		$ret['rating'] = ($movie->rating == 0) ? '' : $movie->rating;
+		$ret['plot'] = $movie->overview;
+		$ret['year'] = date("Y", strtotime($movie->released));
+		$ret['genre'] = '';
+		if (isset($movie->genres) && sizeof($movie->genres) > 0) {
+			$genres = array();
+			foreach($movie->genres as $genre) {
+				$genres[] = $genre->name;
+			}
+			$ret['genre'] = implode(', ', $genres);
+		}
+		$ret['cover'] = '';
+		if (isset($movie->posters) && sizeof($movie->posters) > 0) {
+			foreach($movie->posters as $poster) {
+				if ($poster->image->size == 'cover') {
+					$ret['cover'] = $poster->image->url;
+				}
+			}
+		}
+		$ret['backdrop'] = '';
+		if (isset($movie->backdrops) && sizeof($movie->backdrops) > 0) {
+			foreach($movie->backdrops as $backdrop) {
+				if ($backdrop->image->size == 'original') {
+					$ret['backdrop'] = $backdrop->image->url;
+				}
+			}
+		}
+		return $ret;
+	}
+	
+	/**
+     * fetchImdbProperties()
+     *
+     * @link http://code.google.com/p/moving-pictures/source/browse/trunk/MovingPictures/DataProviders/ScraperScripts/IMDb.xml?spec=svn920&r=920
+     * @param int $imdb_id  imdb id
+     * @return array
+     */
+    private function fetchImdbProperties($imdbId)
+    {
+        $imdb_regex = array(
+            'title'    => '/<title>(.*?)\(.*?<\/title>/i',
+            'plot'     => '/plot\s?(?:outline|summary)?:<\/h5>\s<div.*?>([^<]*)/i',
+            'rating'   => '/<b>([0-9]{1,2}\.[0-9]{1,2})\/10<\/b>/i',
+			'year'     => '/<title>.*?\((\d+).*?<\/title>/i',
+			'genre'    => '/\/Sections\/Genres\/(.+?)\//i',
+			'cover'    => '/<a name="poster".+title=".+">.*?src="([^"]*)"/i'
+        );
+
+        $buffer = file_get_contents("http://www.imdb.com/title/tt$imdbId/");
+
+        // make sure we got some data
+        if (strlen($buffer))
+        {
+        	$ret    = array();
+            $ret['tmdb_id'] = '';
+			$ret['imdb_id'] = $imdbId;
+			$ret['backdrop'] = '';
+			$error = true;
+            foreach ($imdb_regex as $field => $regex)
+            {
+                if (!preg_match($regex, $buffer, $matches))
+                {
+                    //print "Error fetching '$field' for imdb id: $imdbId\n";
+                    $ret[$field] = '';
+                }
+                else 
+                {
+                    $match = $matches[1];
+                    $match = strip_tags(trim(rtrim(addslashes($match))));
+                    $ret[$field] = $match;
+                    $error = false;
+                }
+            }
+            return ($error === true) ? false : $ret;
+        }
+        return false;
+    }
+}
+?>
--- www/lib/releases.php
+++ www/lib/releases.php
@@ -7,6 +7,7 @@ require_once(WWW_DIR."/lib/releaseregex.php");
 require_once(WWW_DIR."/lib/category.php");
 require_once(WWW_DIR."/lib/tvrage.php");
 require_once(WWW_DIR."/lib/nzb.php");
+require_once(WWW_DIR."/lib/nfo.php");
 require_once(WWW_DIR."/lib/zipfile.php");
 
 class Releases
@@ -234,8 +235,8 @@ class Releases
 		$db = new DB();
 		$users = new Users();
 		$s = new Sites();
+		$nfo = new Nfo();
 		$site = $s->get();
-
 		//
 		// delete from disk.
 		//
@@ -243,7 +244,7 @@ class Releases
 		if ($rel && file_exists($site->nzbpath.$rel["guid"].".nzb.gz")) 
 			unlink($site->nzbpath.$rel["guid"].".nzb.gz");
 		
-		$this->deleteReleaseNfo($id);
+		$nfo->deleteReleaseNfo($id);
 		$this->deleteCommentsForRelease($id);
 		$users->delCartForRelease($id);
 		$db->query(sprintf("delete from releases where id = %d", $id));		
@@ -413,6 +414,13 @@ class Releases
 		return $db->queryOneRow(sprintf("select releases.*, groups.name as group_name from releases left outer join groups on groups.ID = releases.groupID where releases.ID = %d ", $id));		
 	}	
 
+	public function getReleaseNfo($id, $incnfo=true)
+	{			
+		$db = new DB();
+		$selnfo = ($incnfo) ? ', uncompress(nfo) as nfo' : '';
+		return $db->queryOneRow(sprintf("SELECT ID, releaseID".$selnfo." FROM releasenfo where releaseID = %d AND nfo IS NOT NULL", $id));		
+	}
+	
 	public function updateGrab($guid)
 	{			
 		$db = new DB();
@@ -426,6 +434,7 @@ class Releases
 		$nzb = new Nzb;
 		$relreg = new ReleaseRegex;
 		$page = new Page;
+		$nfo = new Nfo;
 		$retcount = 0;
 		
 		if (!file_exists($page->site->nzbpath))
@@ -569,10 +578,10 @@ class Releases
 			//
 			// find an .nfo in the release
 			//
-			$relnfo = $this->determineReleaseNfo($nzbdata);
+			$relnfo = $nfo->determineReleaseNfo($nzbdata);
 			if ($relnfo !== false) 
 			{
-				$this->addReleaseNfo($relid, $relnfo['binary']['ID']);
+				$nfo->addReleaseNfo($relid, $relnfo['binary']['ID']);
 				$nfocount++;
 			}
 
@@ -627,7 +636,7 @@ class Releases
 			if ($echooutput)
 				echo "processing nfo files\n";		
 
-			$this->processNfoFiles($echooutput, ($page->site->lookupimdb=="1"));
+			$nfo->processNfoFiles($echooutput, ($page->site->lookupimdb=="1"));
 		}
 		
 		//
@@ -905,257 +914,5 @@ class Releases
 		
 		return $db->query(sprintf(" SELECT releasecomment.*, users.username FROM releasecomment LEFT OUTER JOIN users ON users.ID = releasecomment.userID where userID = %d order by releasecomment.createddate desc ".$limit, $uid));		
 	}
-	
-	public function determineReleaseNfo($nzbdata)
-	{
-		$nfos = array();
-		foreach ($nzbdata as $bin) {
-			if (preg_match('/.*\.nfo[ "\)\]\-]/i', $bin['binary']['name'])) {
-				$nfos[$bin['binary']['name']] = $bin;
-			}
-		}
-		ksort($nfos);
-		return (is_array($nfos) && !empty($nfos)) ? array_shift($nfos) : false;
-	}
-	
-	public function determineReleaseExtras($nzbdata)
-	{
-		$allowedFileExt = array('jpg', 'gif', 'png');
-		$fileExtPattern = implode('|', $allowedFileExt);
-		$files = $typesFound = array();
-		foreach ($nzbdata as $bin) {
-			if (preg_match('/\.('.$fileExtPattern.')[ "\)\]\-]/i', $bin['binary']['name'], $matches)) {
-				if (!in_array($matches[1], $typesFound)) { //optional, only get one of each file ext
-					$bin['type'] = $matches[1];
-					$files[$bin['binary']['name']] = $bin;
-					$typesFound[] = $bin['type'];
-				}
-			}
-		}
-		return (is_array($files) && !empty($files)) ? $files : false;
-	}
-
-	public function addReleaseNfo($relid, $binid)
-	{
-		$db = new DB();
-		return $db->queryInsert(sprintf("INSERT INTO releasenfo (releaseID, binaryID) VALUES (%d, %d)", $relid, $binid));		
-	}
-	
-	public function deleteReleaseNfo($relid)
-	{
-		$db = new DB();
-		return $db->query(sprintf("delete from releasenfo where releaseID = %d", $relid));		
-	}
-	
-	public function getReleaseNfo($id, $incnfo=true)
-	{			
-		$db = new DB();
-		$selnfo = ($incnfo) ? ', uncompress(nfo) as nfo' : '';
-		return $db->queryOneRow(sprintf("SELECT ID, releaseID".$selnfo." FROM releasenfo where releaseID = %d AND nfo IS NOT NULL", $id));		
-	}
-	
-	public function processNfoFiles($echooutput=false, $processImdb = true)
-	{
-		$ret = 0;
-		$db = new DB();
-		$nzb = new Nzb();
-		$nntp = new Nntp();
-	
-		$res = $db->queryDirect(sprintf("SELECT * FROM releasenfo WHERE nfo IS NULL AND attempts < 5"));
-
-		if ($res) 
-		{
-			$nntp->doConnect();
-			while ($arr = mysql_fetch_array($res, MYSQL_BOTH)) 
-			{
-				$binaryToFetch = $nzb->getNZB(array($arr['binaryID']));
-				$fetchedBinary = $nntp->getBinary($binaryToFetch[0]);
-				if ($fetchedBinary !== false) 
-				{
-					if ($processImdb)
-					{
-						//parse nfo for metadata
-						$imdbId = $this->parseImdb($fetchedBinary);
-						if ($imdbId !== false) 
-						{
-							$db->query(sprintf("UPDATE releases SET imdbID = %s WHERE ID = %d", $db->escapeString($imdbId), $arr["releaseID"]));
-							
-
-							// check for existing movie entry
-							$movCheck = $db->queryOneRow(sprintf("SELECT ID FROM movieinfo where imdbID = %d", $imdbId));
-							if ($movCheck === false)
-							{
-								if ($echooutput)
-									echo "fetching imdb info from tmdb - ".$imdbId."\n";
-								
-								// check themoviedb for imdb info
-								$imdb = $this->fetchTvdbProperties($imdbId);
-								if ($imdb === false) {
-									if ($echooutput)
-										echo "not found... trying from imdb - ".$imdbId."\n";
-										
-									// if themoviedb doesnt have it then scrape imdb.com
-									$imdb = $this->fetchImdbProperties($imdbId);
-								}
-								print_r($imdb);
-								if (isset($imdb['cover']) && $imdb['cover'] != '') {
-									$cover = $this->getReleaseImage($imdb['cover']);
-									if ($cover !== false) {
-										$imdb['cover'] = $cover;
-									}
-								}
-								if (isset($imdb['backdrop']) && $imdb['backdrop'] != '') {
-									$backdrop = $this->getReleaseImage($imdb['backdrop']);
-									if ($backdrop !== false) {
-										$imdb['backdrop'] = $backdrop;
-									}
-								}
-								$imdb['tmdb_id'] = ($imdb['tmdb_id'] == '') ? "NULL" : $imdb['tmdb_id'];
-								$query = sprintf("INSERT INTO movieinfo (imdbID, tmdbID, title, rating, plot, year, genre, cover, backdrop, createddate) VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s, NOW())", $imdb['imdb_id'], $imdb['tmdb_id'], $db->escapeString($imdb['title']), $db->escapeString($imdb['rating']), $db->escapeString($imdb['plot']), $db->escapeString($imdb['year']), $db->escapeString($imdb['genre']), $db->escapeString($imdb['cover']), $db->escapeString($imdb['backdrop']));
-								//echo $query;
-								$db->queryInsert($query);
-							}
-
-						}
-					}
-					
-					//insert nfo into database
-					$db->query(sprintf("UPDATE releasenfo SET nfo = compress(%s) WHERE ID = %d", $db->escapeString($fetchedBinary), $arr["ID"]));
-					$ret++;
-				} 
-				else 
-				{
-					if ($echooutput)
-						echo "nfo download failed - release ".$arr['releaseID']." on attempt ".($arr["attempts"]++)."\n";
-						
-					//nfo download failed, increment attempts
-					$db->query(sprintf("UPDATE releasenfo SET attempts = attempts+1 WHERE ID = %d", $arr["ID"]));
-				}
-			}
-			$nntp->doQuit();
-		}
-		
-		if ($echooutput)
-			echo $ret." nfo files processed\n";
-		
-		return $ret;
-	}
-	
-	function getReleaseImage($imgUrl)
-	{		
-		$img = file_get_contents($imgUrl);
-		$im = imagecreatefromstring($img);
-		if($im !== false) {
-			return $img;
-		}
-		return false;	
-	}
-	
-	public function parseImdb($str) {
-		preg_match('/imdb.*?tt(\d{7})/i', $str, $matches);
-		if (isset($matches[1]) && !empty($matches[1])) {
-			return trim($matches[1]);
-		}
-		return false;
-	}
-
-	public function fetchTvdbProperties($imdbId)
-	{
-		require_once(WWW_DIR."/lib/TMDb.php");
-		$tmdb = new TMDb('9a4e16adddcd1e86da19bcaf5ff3c2a3');
-		$lookupId = 'tt'.$imdbId;
-		$movie = array_shift(json_decode($tmdb->getMovie($lookupId, TMDb::IMDB)));
-		if ($movie == 'Nothing found.') { return false; }
-		$ret = array();
-		$ret['title'] = $movie->name;
-		$ret['tmdb_id'] = $movie->id;
-		$ret['imdb_id'] = $imdbId;
-		$ret['rating'] = ($movie->rating == 0) ? '' : $movie->rating;
-		$ret['plot'] = $movie->overview;
-		$ret['year'] = date("Y", strtotime($movie->released));
-		$ret['genre'] = '';
-		if (isset($movie->genres) && sizeof($movie->genres) > 0) {
-			$genres = array();
-			foreach($movie->genres as $genre) {
-				$genres[] = $genre->name;
-			}
-			$ret['genre'] = implode(', ', $genres);
-		}
-		$ret['cover'] = '';
-		if (isset($movie->posters) && sizeof($movie->posters) > 0) {
-			foreach($movie->posters as $poster) {
-				if ($poster->image->size == 'cover') {
-					$ret['cover'] = $poster->image->url;
-				}
-			}
-		}
-		$ret['backdrop'] = '';
-		if (isset($movie->backdrops) && sizeof($movie->backdrops) > 0) {
-			foreach($movie->backdrops as $backdrop) {
-				if ($backdrop->image->size == 'original') {
-					$ret['backdrop'] = $backdrop->image->url;
-				}
-			}
-		}
-		return $ret;
-	}
-	
-    /**
-     * fetchImdbProperties()
-     *
-     * @link http://code.google.com/p/moving-pictures/source/browse/trunk/MovingPictures/DataProviders/ScraperScripts/IMDb.xml?spec=svn920&r=920
-     * @param int $imdb_id  imdb id
-     * @return array
-     */
-    private function fetchImdbProperties($imdb_id = 0)
-    {
-        // :-)
-        if ((int)$imdb_id > 0)
-        {
-            if (is_callable('file_get_contents'))
-            {
-                $imdb_regex = array(
-                    'title'    => '/<title>(.*?)\(.*?<\/title>/i',
-                    'plot'     => '/plot\s?(?:outline|summary)?:<\/h5>\s<div.*?>([^<]*)/i',
-                    'rating'   => '/<b>([0-9]{1,2}\.[0-9]{1,2})\/10<\/b>/i',
-					'year'     => '/<title>.*?\((\d+).*?<\/title>/i',
-					'genre'    => '/\/Sections\/Genres\/(.+?)\//i',
-					'cover'    => '/<a name="poster".+title=".+">.*?src="([^"]*)"/i'
-                );
-				
-                $ret    = array();
-                $ret['tmdb_id'] = '';
-				$ret['imdb_id'] = $imdb_id;
-				$ret['backdrop'] = '';
-		
-                $buffer = file_get_contents("http://www.imdb.com/title/tt$imdb_id/");
-
-                // make sure we got some data
-                if (strlen($buffer))
-                {
-                    foreach ($imdb_regex as $field => $regex)
-                    {
-                        if (!preg_match($regex, $buffer, $matches))
-                        {
-                            print "Error fetching '$field' for imdb id: $imdb_id\n";
-                            $ret[$field] = '';
-                        }
-                        else
-                        {
-                            $match = $matches[1];
-                            $match = strip_tags(trim(rtrim(addslashes($match))));
-                            $ret[$field] = $match;
-                        }
-                    }
-                }
-
-                return $ret;
-            }
-            else
-            {
-                print __FUNCTION__ . ": Unable to call file_get_contents()! - Perhaps it was removed by your host.\n";
-            }
-        }
-    }
 }
 ?>
