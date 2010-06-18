bf48b3165d666db71b39d84ebda0c3f0450e0f2f
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-06-18 21:56:00 +0000

    add movie info from admin
    git-svn-id: svn://svn.newznab.com/nn/trunk@395 a8b8df2f-1e19-4095-b43c-2e6d618ca476

---
+++ www/admin/movie-add.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once("config.php");
+require_once(WWW_DIR."/lib/adminpage.php");
+require_once(WWW_DIR."/lib/movie.php");
+
+$page = new AdminPage();
+$movie = new Movie();
+$id = 0;
+
+$page->title = "Movie Add";
+
+if (isset($_REQUEST['id']) && ctype_digit($_REQUEST['id']) && strlen($_REQUEST['id']) == 7) {
+	$id = $_REQUEST['id'];
+	if($movie->updateMovieInfo($id)) {
+		header("Location:".WWW_TOP."/movie-list.php");
+		die();
+	}
+}
+
+$page->content = $page->smarty->fetch('admin/movie-add.tpl');
+$page->render();
+
+?>
