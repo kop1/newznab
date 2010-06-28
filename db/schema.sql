81c75fff9ee8ce82f1b4780bea57b46e94b2ad7a
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-06-28 15:07:52 +0000

    accidently removed fulltext index from binaries, which ruined rawsearch on large volumes of data - to patch run -
    alter table binaries drop index ix_binary_name;
    CREATE FULLTEXT INDEX ix_binary_name ON binaries (name);
    
    git-svn-id: svn://svn.newznab.com/nn/trunk@439 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE `binaries` (
 		KEY `groupID` (`groupID`)
 		) ENGINE=MYISAM AUTO_INCREMENT=1 ;
 
-CREATE INDEX ix_binary_name ON binaries (name);
+CREATE FULLTEXT INDEX ix_binary_name ON binaries (name);
 CREATE INDEX ix_binary_relname ON binaries (relname);
 CREATE INDEX ix_binary_procstat ON binaries (procstat);
 CREATE INDEX ix_binary_releaseID ON binaries (releaseID);
