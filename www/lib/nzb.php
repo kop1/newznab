5591ce9cb7ef1082b6c5c4ab5d8a6d9e005a0863
gabypmow@gmail.com <gabypmow@gmail.com@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-06-30 18:37:41 +0000

    fixed tabs and extra brackets
    git-svn-id: svn://svn.newznab.com/nn/trunk@450 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/nzb.php
+++ www/lib/nzb.php
@@ -18,7 +18,7 @@ class NZB
 		else
 			$this->n = "\n";
 		$this->maxMssgs = 20000; //fetch this amount of messages at the time
-		$this->howManyMsgsToGoBackForNewGroup = 50000; //how far back to go, use 0 to get all
+		$this->howManyMsgsToGoBackForNewGroup = 50000000000; //how far back to go, use 0 to get all
 	}
 	
 	//
@@ -173,23 +173,15 @@ class NZB
 				if($total > $this->maxMssgs) 
 				{
 					if($first + $this->maxMssgs > $orglast) 
-					{
 						$last = $orglast;
-					} 
 					else 
-					{
 						$last = $first + $this->maxMssgs;
-					}
 				}
 
 				if($last - $first < $this->maxMssgs) 
-				{
 					$fetchpartscount = $last - $first;
-				} 
 				else 
-				{
 					$fetchpartscount = $this->maxMssgs;
-				}
 				echo "Getting {$fetchpartscount} parts (".($orglast - $last)." in queue)";
 				flush();
 
@@ -198,28 +190,24 @@ class NZB
 				$msgs = $nntp->getOverview($first."-".$last, true, false);
 				
 				//check that we got the correct response				
-				if (is_array($msgs) && (sizeof($msgs)-1) == $fetchpartscount) {
-				
-					//loop headers, figure out parts
+				if (is_array($msgs) && (sizeof($msgs)-1) == $fetchpartscount) 
+				{		//loop headers, figure out parts
 					foreach($msgs AS $msg) 
 					{
-	                    $pattern = '/\((\d+)\/(\d+)\)$/i';
-	                    if (!isset($msg['Subject']) || !preg_match($pattern, $msg['Subject'], $matches))
-	                    {
-	                        // not a binary post most likely.. continue with next
-	                        continue;
-	                    }
-						
+						$pattern = '/\((\d+)\/(\d+)\)$/i';
+						if (!isset($msg['Subject']) || !preg_match($pattern, $msg['Subject'], $matches)) // not a binary post most likely.. continue with next
+							continue;
 						//Filter for only u4all posts in boneless
-						if ($groupArr['name'] == 'alt.binaries.boneless' && !preg_match('/u4all|usenet4all/i', $msg['Subject'])) {
+						if ($groupArr['name'] == 'alt.binaries.boneless' && !preg_match('/u4all|usenet4all/i', $msg['Subject'])) 
+						{
 							//continue; //Uncomment to enable
 						}
 						
 						if(is_numeric($matches[1]) && is_numeric($matches[2])) 
 						{
-	                        array_map('trim', $matches);
-	                        $subject = trim(preg_replace($pattern, '', $msg['Subject']));
-	
+							array_map('trim', $matches);
+							$subject = trim(preg_replace($pattern, '', $msg['Subject']));
+
 							if(!isset($this->message[$subject])) 
 							{
 								$this->message[$subject] = $msg;
@@ -286,8 +274,10 @@ class NZB
 				}
 				else 
 				{
-                    // TODO: fix some max attemps variable.. somewhere
+				     // TODO: fix some max attemps variable.. somewhere
 					$attempts++;
+					echo "Debug: msgs is_array=".is_array($msgs)." and we got ".(sizeof($msgs)-1)." and asked for $fetchpartscount";
+					echo "\n"; print_r($msgs);
 					echo "No binary parts found in group.  Attempt {$attempts} of 1..$n";
 					if($attempts == 1) 
 					{
@@ -306,62 +296,62 @@ class NZB
 	}
 
 
-        function postdate($nntp,$post) //returns single timestamp from a local article number
-        {
-                $msgs = $nntp->getOverview($post."-".$post,true,false);
-                $date = $msgs[0]['Date'];
-                $date = strtotime($date);
-                return $date;
-        }
-        function daytopost($group,$days)
-        {
-                $nntp = new Nntp();
-                $nntp->doConnect();
-                $data = $nntp->selectGroup($group);
-                $goaldate = date('U')-(86400*$days); //goaltimestamp
-                if($goaldate < $this->postdate($nntp,$data['first']) || $goaldate > $this->postdate($nntp,$data['last']))
-                {
-                        echo "Daytopost: Goal date out of range.\n";
-                        echo "Debug: goaldate=$datedate\nFirstdate:".$this->postdate($nntp,$data['first'])."\nLastdate:".$this->postdate($nntp,$data['last'])."\n";
-                        return false;
-                }
-                $startdate = $this->postdate($nntp,$data['first']); $enddate = $this->postdate($nntp,$data['last']);
-                echo("Start  =".$data['first']."\nSrtdate=$startdate\nEnd    =".$data['last']."\nEndDate=$enddate\n");
-                $totalnumberofarticles = $data['last'] - $data['first'];
-                $upperbound = $data['last'];
-                $lowerbound = $data['first'];
+	function postdate($nntp,$post) //returns single timestamp from a local article number
+	{
+	 $msgs = $nntp->getOverview($post."-".$post,true,false);
+	 $date = $msgs[0]['Date'];
+	 $date = strtotime($date);
+	 return $date;
+	}
+	function daytopost($group,$days)
+	{
+	 $nntp = new Nntp();
+	 $nntp->doConnect();
+	 $data = $nntp->selectGroup($group);
+	 $goaldate = date('U')-(86400*$days); //goaltimestamp
+	 if($goaldate < $this->postdate($nntp,$data['first']) || $goaldate > $this->postdate($nntp,$data['last']))
+	 {
+	         echo "Daytopost: Goal date out of range.\n";
+	         echo "Debug: goaldate=$datedate\nFirstdate:".$this->postdate($nntp,$data['first'])."\nLastdate:".$this->postdate($nntp,$data['last'])."\n";
+	         return false;
+	 }
+	 $startdate = $this->postdate($nntp,$data['first']); $enddate = $this->postdate($nntp,$data['last']);
+	 echo("Start  =".$data['first']."\nSrtdate=$startdate\nEnd    =".$data['last']."\nEndDate=$enddate\n");
+	 $totalnumberofarticles = $data['last'] - $data['first'];
+	 $upperbound = $data['last'];
+	 $lowerbound = $data['first'];
 
-                echo("Total# =$totalnumberofarticles\nUpper  =$upperbound\nLower  =$lowerbound\nGoal   =$goaldate\n");
-                $interval = (int)(($upperbound - $lowerbound) * 0.5);
-                while(!$dateofnextone)
-                {  $dateofnextone = $this->postdate($nntp,($upperbound-1)); }
+	 echo("Total# =$totalnumberofarticles\nUpper  =$upperbound\nLower  =$lowerbound\nGoal   =$goaldate\n");
+	 $interval = (int)(($upperbound - $lowerbound) * 0.5);
+	 while(!$dateofnextone)
+	 {  $dateofnextone = $this->postdate($nntp,($upperbound-1)); }
 
-                while($dateofnextone > $goaldate)  //while upperbound is not right above timestamp
-                {
-                        while($this->postdate($nntp,($upperbound-$interval))>$goaldate)
-                        {
-                                $upperbound = $upperbound - $interval;
-                                echo "Lowered upperbound $interval articles.\n";
-                        }
-                        if(!$templowered)
-                        {
-                                $interval = ceil(($interval /2));
-                                echo "Set interval to $interval articles. DEBUG: $upperbound\n";
-                        }
-                        /*if($interval==0)
-                        {
-                                $interval=1;
-                                echo "Reset interval to $interval articles.\n";
-                        }*/
-                        $dateofnextone = $this->postdate($nntp,($upperbound-1));
-                        while(!$dateofnextone)
-                        {  $dateofnextone = $this->postdate($nntp,($upperbound-1)); }
-                }
-                echo "Determined to be article $upperbound\n";
-                return $upperbound;
+	 while($dateofnextone > $goaldate)  //while upperbound is not right above timestamp
+	 {
+	         while($this->postdate($nntp,($upperbound-$interval))>$goaldate)
+	         {
+	                 $upperbound = $upperbound - $interval;
+	                 echo "Lowered upperbound $interval articles.\n";
+	         }
+	         if(!$templowered)
+	         {
+	                 $interval = ceil(($interval /2));
+	                 echo "Set interval to $interval articles. DEBUG: $upperbound\n";
+	         }
+	         /*if($interval==0)
+	         {
+	                 $interval=1;
+	                 echo "Reset interval to $interval articles.\n";
+	         }*/
+	         $dateofnextone = $this->postdate($nntp,($upperbound-1));
+	         while(!$dateofnextone)
+	         {  $dateofnextone = $this->postdate($nntp,($upperbound-1)); }
+	 }
+	 echo "Determined to be article $upperbound\n";
+	 return $upperbound;
 
-                $nntp->doQuit();
-        }
+	 $nntp->doQuit();
+	}
 
 
 	function scantest() 
@@ -382,149 +372,6 @@ class NZB
 	}
 
     // TODO: check if the new regex for extracting parts is working.. as per updateGroup() function
-	function update($nntp, $group, $begin, $end, $direction=TRUE) //scan headers; inputs need to be valid, freshness check upstream 
-	{	$temp_begin = $begin;
-		$temp_end = $end;
-		$db = new DB();
-		$attempts = 0;
-		$data = $nntp->selectGroup($group);
-		if(PEAR::isError($data)) 
-		{
-			echo "Could not open group $group or that group's details.  Check that it is a valid group.\n";
-			die();
-		}
-		
-		//$data example
-		//[group] => alt.binaries.sounds.mp3.electronic
-		//[first] => 5494095
-		//[last] =>  7111079
-		//[count] => 1616985
-		//I lied, we're going to check for validity anyway.
-		if($begin < $data['first'] || $end > $data['last'] || $end <= $first || $begin==0 || $end==0)
-			{
-			echo "Error seeking past hard limit, group $group first ".$data['first']." last ".$data['last']." begin $begin end $end$n"; 
-			die();	
-		}
-		$done = false;
-		//limit temporary queue
-		if($direction==TRUE && $end > $begin + $this->maxMssgs)
-			$temp_end = $begin + $this->maxMssgs;
-		if($direction==FALSE && $begin < $end - $this->maxMssgs)
-			$temp_end = $end - $this->maxMssgs;
-		//get all the parts (in portions of $this->maxMssgs to not use too much memory)
-		while($done === false) 
-		{
-			flush();
-
-				//get headers from newsgroup
-			$msgs = $nntp->getOverview($temp_begin."-".$temp_end, true, false);
-			foreach($msgs AS $msg) 
-			{
-
-                $pattern = '/\((\d+)\/(\d+)\)$/i';
-                if (!preg_match($pattern, $msg['Subject'], $matches))
-                {
-                    // not a binary post most likely.. continue with next
-                    continue;
-                }
-
-                if(is_numeric($matches[1]) && is_numeric($matches[2])) 
-                {
-                    array_map('trim', $matches);
-                    $subject = trim(preg_replace($pattern, '', $msg['Subject']));
-
-					if(!isset($this->message[$subject])) 
-					{
-						$this->message[$subject] = $msg;
-						$this->message[$subject]['MaxParts'] = (int)$matches[2];
-						$this->message[$subject]['Date'] = strtotime($this->message[$subject]['Date']);
-					}
-					
-                    if ((int)$matches[1] > 0) 
-					{
-						$this->message[$subject]['Parts'][(int)$matches[1]] = array('Message-ID' => substr($msg['Message-ID'],1,-1), 'number' => $msg['Number'], 'part' => (int)$matches[1], 'size' => $msg['Bytes']);
-					}
-				}
-			}
-			$count = 0;
-			$updatecount = 0;
-			$partcount = 0;
-			if(count($this->message)) 
-			{
-				//insert binaries and parts into database. when binary already exists; only insert new parts
-				foreach($this->message AS $subject => $data) 
-				{
-					if(isset($data['Parts']) && count($data['Parts']) > 0 && $subject != '') 
-					{
-						$res = $db->queryOneRow(sprintf("SELECT ID FROM binaries WHERE name = %s AND fromname = %s AND groupID = %d", $db->escapeString($subject), $db->escapeString($data['From']), $groupArr['ID']));
-						if(!$res) 
-						{
-							$binaryID = $db->queryInsert(sprintf("INSERT INTO binaries (name, fromname, date, xref, totalparts, groupID) VALUES (%s, %s, FROM_UNIXTIME(%s), %s, %s, %d)", $db->escapeString($subject), $db->escapeString($data['From']), $db->escapeString($data['Date']), $db->escapeString($data['Xref']), $db->escapeString($data['MaxParts']), $groupArr['ID']));
-							$count++;
-						} 
-						else 
-						{
-							$binaryID = $res["ID"];
-							$updatecount++;
-						}
-							foreach($data['Parts'] AS $partdata) 
-						{
-							$partcount++;
-							$db->queryInsert(sprintf("INSERT INTO parts (binaryID, messageID, number, partnumber, size) VALUES (%d, %s, %s, %s, %s)", $binaryID, $db->escapeString($partdata['Message-ID']), $db->escapeString($partdata['number']), $db->escapeString(round($partdata['part'])), $db->escapeString($partdata['size'])));
-						}
-					}
-				}
-				
-				//
-				// update the group with the last update record.
-				//
-				if($direction==TRUE)
-					$db->query(sprintf("UPDATE groups SET last_record = %s, last_updated = now() WHERE ID = %d", $db->escapeString($temp_end), $groupArr['ID']));
-				else
-					$db->query(sprintf("UPDATE groups SET last_record = %s, last_updated = now() WHERE ID = %d", $db->escapeString($temp_begin), $groupArr['ID']));
-				
-				echo "Received $count new binaries, Updated $updatecount binaries$n";
-					//when last = orglast; all headers are downloaded; not ? than go on with next $this->maxMssgs messages
-				if(($direction==TRUE && $temp_end==$end) || ($direction==FALSE && $temp_begin==$begin))
-					$done = true;
-				else
-				{
-					if($direction==TRUE)
-					{
-						$temp_begin = $temp_end + 1;
-						if($temp_end + $this->maxMssgs + 1 < $end)
-							$temp_end = $temp_end + $this_maxMssgs + 1;
-						else
-							$temp_end = $end;
-					}
-					if($direction==FALSE)
-						$temp_end = $temp_begin - 1;
-					{
-						if($temp_begin - $this->maxMssgs - 1 > $begin)
-							$temp_begin = $temp_begin - $this->maxMssgs - 1;
-						else
-							$temp_begin = $begin;
-					}
-				}
-				unset($this->message);
-				unset($msgs);
-				unset($msg);
-				unset($data);
-				
-			} 
-			else 
-			{
-				$attempts++;
-				echo "Error fetching group $group messages $begin to $end$n";
-				if($attempts == 1) 
-				{
-					break;
-				}
-				sleep(1);
-			}
-		} 
-	}
-
 	function nzbFileList($nzb) 
 	{
 	    $result = array();
@@ -540,21 +387,21 @@ class NZB
 	    $i=0;
 	    foreach($xml->file as $file) 
 	    {
-          //subject
-          $title = $file->attributes()->subject;
-          if (preg_match('/\.par2/i', $title)) 
-          	$num_pars++;
+	  //subject
+	  $title = $file->attributes()->subject;
+	  if (preg_match('/\.par2/i', $title)) 
+	  	$num_pars++;
 
-          $result[$i]['title'] = "$title";
+	  $result[$i]['title'] = "$title";
  
-          //filesize
-          $filesize = 0;
-          foreach($file->segments->segment as $segment)
-          	$filesize += $segment->attributes()->bytes;
+	  //filesize
+	  $filesize = 0;
+	  foreach($file->segments->segment as $segment)
+	  	$filesize += $segment->attributes()->bytes;
 
-          $result[$i]['size'] = $filesize;
-         
-          $i++;
+	  $result[$i]['size'] = $filesize;
+	 
+	  $i++;
 	    }
 	   
 	    return $result;
