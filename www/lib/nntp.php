164f51b52c00cbffc4d639160ebaea618a238e1a
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-07-02 23:00:48 +0000

    cleanup xzver functions
    git-svn-id: svn://svn.newznab.com/nn/trunk@461 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/nntp.php
+++ www/lib/nntp.php
@@ -77,50 +77,17 @@ class Nntp extends Net_NNTP_Client
 		return $message;
 	}
 	
-	function getXOverview($range = null, $_names = true, $_forceNames = true)
+	function getXOverview($range, $_names = true, $_forceNames = true)
     {
-    	// API v1.0
-    	switch (true) {
-	    // API v1.3
-	    case func_num_args() != 2:
-	    case is_bool(func_get_arg(1)):
-	    case !is_int(func_get_arg(1)) || (is_string(func_get_arg(1)) && ctype_digit(func_get_arg(1))):
-	    case !is_int(func_get_arg(0)) || (is_string(func_get_arg(0)) && ctype_digit(func_get_arg(0))):
-		break;
-
-	    default:
-    	    	// 
-    	        trigger_error('You are using deprecated API v1.0 in Net_NNTP_Client: getOverview() !', E_USER_NOTICE);
-
-    	        // Fetch overview via API v1.3
-    	        $overview = $this->getOverview(func_get_arg(0) . '-' . func_get_arg(1), true, false);
-    	        if (PEAR::isError($overview)) {
-    	            return $overview;
-    	        }
-
-    	        // Create and return API v1.0 compliant array
-    	        $articles = array();
-    	        foreach ($overview as $article) {
-
-    	    	    // Rename 'Number' field into 'number'
-    	    	    $article = array_merge(array('number' => array_shift($article)), $article);
-		
-    	    	    // Use 'Message-ID' field as key
-    	            $articles[$article['Message-ID']] = $article;
-    	        }
-    	        return $articles;
-    	}
-
     	// Fetch overview from server
     	$overview = $this->cmdXZver($range);
     	if (PEAR::isError($overview)) {
     	    return $overview;
     	}
-    	//print_r($overview);
     	
     	// Use field names from overview format as keys?
-    	if ($_names) {
-
+    	if ($_names) 
+    	{
     	    // Already cached?
     	    if (is_null($this->_overviewFormatCache)) {
     	    	// Fetch overview format
@@ -134,45 +101,43 @@ class Nntp extends Net_NNTP_Client
 
     	    	// Cache format
     	        $this->_overviewFormatCache = $format;
-
-    	    // 
-    	    } else {
+    	    } 
+    	    else 
+    	    {
     	        $format = $this->_overviewFormatCache;
     	    }
 
     	    // Loop through all articles
             foreach ($overview as $key => $article) 
             {
-
-    	        // Copy $format
-    	        $f = $format;
-
-    	        // Field counter
-    	        $i = 0;
-		
-							// Loop through forld names in format
-    	        foreach ($f as $tag => $full) 
-    	        {
-								if (isset($article[$i + 1]))
-								{
-    	    	    //
-    	            $f[$tag] = $article[$i++];
-
-    	            // If prefixed by field name, remove it
-    	            if ($full === true) 
-    	            {
-	                	$f[$tag] = ltrim( substr($f[$tag], strpos($f[$tag], ':') + 1), " \t");
-    	            }
-    	          }
-    	        }
-
+				// Copy $format
+				$f = $format;
+				
+				// Field counter
+				$i = 0;
+				
+				// Loop through forld names in format
+				foreach ($f as $tag => $full) 
+				{
+					if (isset($article[$i + 1]))
+					{
+					    //
+				        $f[$tag] = $article[$i++];
+				
+				        // If prefixed by field name, remove it
+				        if ($full === true) 
+				        {
+				        	$f[$tag] = ltrim( substr($f[$tag], strpos($f[$tag], ':') + 1), " \t");
+				        }
+					}
+				}
     	        // Replace article 
-	        $overview[$key] = $f;
+	        	$overview[$key] = $f;
     	    }
     	}
 
-    	switch (true) {
-
+    	switch (true)
+    	{
     	    // Expect one article
     	    case is_null($range);
     	    case is_int($range);
