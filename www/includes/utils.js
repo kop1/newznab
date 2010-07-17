528df214b48a9586ca21f5282ce8694f660fb24a
sir.downloadus <sir.downloadus@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-07-17 03:05:25 +0000

    prevent NFO modals from adjusting height when opened
    git-svn-id: svn://svn.newznab.com/nn/trunk@501 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/includes/utils.js
+++ www/includes/utils.js
@@ -37,12 +37,12 @@ jQuery(function($){
 	$("table.data a.modal_nfo").colorbox({	 // NFO modal
 		href: function(){ return $(this).attr('href') +'&modal'; },
 		title: function(){ return $(this).parent().parent().children('a.title').text(); },
-		width:"800px", height:"90%", initialWidth:"800px", initialHeight:"90%", speed:0, opacity:0.7
+		innerWidth:"800px", innerHeight:"90%", initialWidth:"800px", initialHeight:"90%", speed:0, opacity:0.7
 	});
 	$("table.data a.modal_imdb").colorbox({	 // IMDB modal
 		href: function(){ return $(this).attr('name') +'&modal'; },
 		title: function(){ return $(this).parent().parent().children('a.title').text(); },
-		width:"800px", height:"450px", initialWidth:"800px", initialHeight:"450px", speed:0, opacity:0.7
+		innerWidth:"800px", innerHeight:"450px", initialWidth:"800px", initialHeight:"450px", speed:0, opacity:0.7
 	}).click(function(){
 		$('#colorbox').removeClass().addClass('cboxMovie');	
 	});
