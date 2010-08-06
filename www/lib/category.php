01d7839531f76b57dbee08be88f5287d5e246a25
brannon.bowden@gmail.com <brannon.bowden@gmail.com@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-08-06 19:42:46 +0000

    Added more sports to category.php
    git-svn-id: svn://svn.newznab.com/nn/trunk@559 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/category.php
+++ www/lib/category.php
@@ -228,6 +228,8 @@ class Category
 			if (preg_match('/MMA\./', $releasename)) { return Category::CAT_TV_SPORT; }
 			if (preg_match('/UFC\./', $releasename)) { return Category::CAT_TV_SPORT; }
 			if (preg_match('/FIA\./', $releasename)) { return Category::CAT_TV_SPORT; }
+			if (preg_match('/PGA\./', $releasename)) { return Category::CAT_TV_SPORT; }
+			if (preg_match('/NFL\./', $releasename)) { return Category::CAT_TV_SPORT; }
 			if (preg_match('/Rugby\./', $releasename)) { return Category::CAT_TV_SPORT; }
 			if (preg_match('/TNA\./', $releasename)) { return Category::CAT_TV_SPORT; }
 			if (preg_match('/EPL\.(\d{4})/', $releasename)) { return Category::CAT_TV_SPORT; }
