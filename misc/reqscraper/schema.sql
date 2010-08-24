099e62be2cc02850b89bc52996dc2bb12ffd9003
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-08-24 13:24:16 +0000

    add scrape and query functionality to reqscraper
    git-svn-id: svn://svn.newznab.com/nn/trunk@677 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/reqscraper/schema.sql
+++ misc/reqscraper/schema.sql
@@ -1,3 +1,37 @@
-/*
-db sql to create schema goes here.
-*/
+DROP TABLE IF EXISTS `feed`;
+CREATE TABLE `feed` 
+(
+	`ID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`code` VARCHAR(50) NULL,
+	`name` VARCHAR(255) NULL,
+	`url` VARCHAR(1000) NOT NULL,
+	`lastupdate` DATETIME NULL,
+	`status` INT NOT NULL DEFAULT 1,
+	PRIMARY KEY  (`ID`)
+) ENGINE=MYISAM AUTO_INCREMENT=1 ;
+
+CREATE INDEX ix_feed_code ON feed (code);
+
+insert into feed (code, name, url, lastupdate) values ('tv', 'abteevee', 'http://abteevee.allfilled.com/rss.php', null);
+insert into feed (code, name, url, lastupdate) values ('xxx', 'aberotica', 'http://aberotica.allfilled.com/rss.php', null);
+insert into feed (code, name, url, lastupdate) values ('abgx', 'abgx', 'http://www.abgx.net/rss/abgw/reqs.rss', null);
+
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` 
+(
+	`ID` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`feedID` INT NOT NULL,
+	`reqid` INT NOT NULL,
+	`title` VARCHAR(255) NULL,
+	`link` VARCHAR(1000) NULL,
+	`description` VARCHAR(1000) NULL,
+	`pubdate` DATETIME NOT NULL,
+	`guid` VARCHAR(50) NULL,
+	`adddate` DATETIME NOT NULL,
+	PRIMARY KEY  (`ID`)
+) ENGINE=MYISAM AUTO_INCREMENT=1 ;
+
+CREATE INDEX ix_item_feedID ON item (feedID);
+CREATE INDEX ix_item_reqid ON item (reqid);
+CREATE UNIQUE INDEX ix_item_guid ON item (guid);
--- misc/reqscraper/scrape.php
+++ misc/reqscraper/scrape.php
@@ -6,15 +6,33 @@ require_once('config.php');
 //
 // retrieve a list of feeds to be scraped
 //
-$rss = fetch_rss("http://abteevee.allfilled.com/rss.php");
+$result = mysql_query("select * from feed where status=1");	
+while ($row = mysql_fetch_array($result, MYSQL_BOTH)) 
+{
+	$rss = fetch_rss($row["url"]);
 
-//
-// scrape every item into a database table
-//
+	//
+	// scrape every item into a database table
+	//
+	foreach ($rss->items as $item) 
+	{
+		$link = mysql_real_escape_string($item['link']);
+		$title = mysql_real_escape_string($item['title']);	
+		$description = mysql_real_escape_string($item['description']);	
+		$feedID = $row["ID"];
+		$pubdate = date("Y-m-d H:i:s", strtotime($item['pubdate']));
+		$guid = mysql_real_escape_string($item['guid']);	
 
-//
-// store 'specific stuff' like parsed reqids by regexing
-//
+		//
+		// store 'specific stuff' like parsed reqids by regexing
+		//
+		$reqid = 0;
+		$matches = "";
+		if (preg_match("/^ReqId: (?P<reqid>\d{3,6})/i", $description, $matches))
+			$reqid = mysql_real_escape_string($matches["reqid"]);	
 
+		$res = mysql_query("insert into item (feedID, reqid, title, link, description, pubdate, guid, adddate) values ($feedID, '$reqid', '$title', '$link', '$description', '$pubdate', '$guid', now())");	
+	}
+}
 
 ?>
