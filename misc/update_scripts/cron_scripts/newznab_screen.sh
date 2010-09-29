479878b8cf922567b6fe1b35a1b920dcf7432b12
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-09-29 22:41:47 +0000

    run db optimize every 24 hours
    git-svn-id: svn://svn.newznab.com/nn/trunk@878 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/update_scripts/cron_scripts/newznab_screen.sh
+++ misc/update_scripts/cron_scripts/newznab_screen.sh
@@ -5,13 +5,24 @@ set -e
 
 export NEWZNAB_PATH="/usr/local/www/newznab/misc/update_scripts"
 export NEWZNAB_SLEEP_TIME="600" # in seconds
+LASTOPTIMIZE=`date +%s`
 
 while :
 
  do
+CURRTIME=`date +%s`
 cd ${NEWZNAB_PATH}
 /usr/bin/php5 ${NEWZNAB_PATH}/update_binaries.php
 /usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php
+
+DIFF=$[$CURRTIME-$LASTOPTIMIZE]
+if [ $DIFF -gt 86400 ]; then
+	LASTOPTIMIZE=`date +%s`
+	echo "Optimizing DB..."
+	/usr/bin/php5 ${NEWZNAB_PATH}/optimise_db.php
+fi
+
+echo "waiting ${NEWZNAB_SLEEP_TIME} seconds..."
 sleep ${NEWZNAB_SLEEP_TIME}
 
 done
