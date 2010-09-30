67988f7329ecacfa56faf45794a0f38d53cb8d43
brannon.bowden@gmail.com <brannon.bowden@gmail.com@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-09-30 01:25:56 +0000

    Updated Batch Files to add in Optimise DB to the runme.bat file, also created stand alone batch file for optimise db.
    git-svn-id: svn://svn.newznab.com/nn/trunk@880 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/update_scripts/batch_scripts/runme.bat
+++ misc/update_scripts/batch_scripts/runme.bat
@@ -1,3 +1,7 @@
+set a=1
+
+set limit=111111111111111111111111
+
 :Top
 
 CD..
@@ -5,6 +9,24 @@ CD..
 php.exe update_binaries.php
 
 php.exe update_releases.php
+
+CD batch_scripts
+
+sleep 120
+
+if %a%==300 goto optimise
+
+set /a a=%a%+1
+
+GOTO TOP
+
+:Optimise
+
+CD..
+
+php.exe optimise_db.php
+
+set a=1
 
 CD batch_scripts
 
