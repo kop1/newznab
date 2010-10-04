096f37deaaaa416d5403bbb9820ee3a308165f98
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-10-04 11:03:40 +0000

    remove comments
    git-svn-id: svn://svn.newznab.com/nn/trunk@891 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- db/patch/0.1.5/2010_10_04_moveadsensetodb.sql
+++ db/patch/0.1.5/2010_10_04_moveadsensetodb.sql
@@ -1,15 +1,1 @@
 alter table site add google_adsense_acc varchar(255) null;
-
-
--- this should all be removed from your config.php
---
---define('GOOGLE_ADSENSE_ACC', '');
-
---define('INSTALL_CHECK', false);
-
---//=========================
---// Config you can leave alone
---//=========================
---define('WWW_DIR', str_replace("\\","/",dirname(__FILE__))."/");
---define('SMARTY_DIR', WWW_DIR.'lib/smarty/');
---
