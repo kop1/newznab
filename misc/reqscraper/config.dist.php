cca7f22c04d632dd6876cfdfbfc8fce5d5361488
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2010-10-11 10:01:34 +0000

    disable magpie rss caching of allfilled scrapes
    git-svn-id: svn://svn.newznab.com/nn/trunk@921 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- misc/reqscraper/config.dist.php
+++ misc/reqscraper/config.dist.php
@@ -5,6 +5,8 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'allfilled');
 define('DB_PASSWORD', 'allfilled');
 define('DB_NAME', 'allfilled');
+
+define('MAGPIE_CACHE_ON', 0);
  
 mysql_pconnect(DB_HOST, DB_USER, DB_PASSWORD)
         or die("fatal error: could not connect to database!");
