4fa14b912345a0e0a43bc75491ea2cfb78a9905d
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-01-06 11:10:46 +0000

    remove template text from license
    git-svn-id: svn://svn.newznab.com/nn/trunk@1115 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/site.php
+++ www/lib/site.php
@@ -159,8 +159,7 @@ class Sites
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.".$n."
+the Free Software Foundation.".$n."
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
