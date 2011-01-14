67bf50339e299ee45ab1c6a155b3487c72d7b8f3
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-01-14 15:50:40 +0000

    add changelog
    git-svn-id: svn://svn.newznab.com/nn/trunk@1178 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/amazon.php
+++ www/lib/amazon.php
@@ -29,7 +29,7 @@
     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
 	
-		http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/BasicAuthProcess.html
+	http://docs.amazonwebservices.com/AWSECommerceService/latest/DG/BasicAuthProcess.html
 
     */
  	
