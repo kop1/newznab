1b1fb41e292564fcba508fbdb0071b2d8344c119
gbandit <gbandit@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-02-01 22:26:03 +0000

    fix magic quotes and arrays in requests.
    git-svn-id: svn://svn.newznab.com/nn/trunk@1267 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/lib/framework/basepage.php
+++ www/lib/framework/basepage.php
@@ -26,11 +26,11 @@ class BasePage
 	
 		if((function_exists("get_magic_quotes_gpc") && get_magic_quotes_gpc()) || ini_get('magic_quotes_sybase'))
 		{
-            foreach($_GET as $k => $v) $_GET[$k] = stripslashes($v);
-            foreach($_POST as $k => $v) $_POST[$k] = stripslashes($v);
-            foreach($_REQUEST as $k => $v) $_REQUEST[$k] = stripslashes($v);
-            foreach($_COOKIE as $k => $v) $_COOKIE[$k] = stripslashes($v);
-        }	
+            foreach($_GET as $k => $v) $_GET[$k] = (is_array($v)) ? array_map("stripslashes", $v) : stripslashes($v);
+            foreach($_POST as $k => $v) $_POST[$k] = (is_array($v)) ? array_map("stripslashes", $v) : stripslashes($v);
+            foreach($_REQUEST as $k => $v) $_REQUEST[$k] = (is_array($v)) ? array_map("stripslashes", $v) : stripslashes($v);
+            foreach($_COOKIE as $k => $v) $_COOKIE[$k] = (is_array($v)) ? array_map("stripslashes", $v) : stripslashes($v);
+        }
 		
 		$this->smarty = new Smarty();
 
