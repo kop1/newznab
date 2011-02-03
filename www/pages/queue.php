36460e494e74a21d24c23ed970fb382430597bf8
barnseenio <barnseenio@a8b8df2f-1e19-4095-b43c-2e6d618ca476>
2011-02-03 12:16:41 +0000

    delete items from sab queue
    git-svn-id: svn://svn.newznab.com/nn/trunk@1284 a8b8df2f-1e19-4095-b43c-2e6d618ca476

--- www/pages/queue.php
+++ www/pages/queue.php
@@ -1,13 +1,21 @@
 <?php
+require_once(WWW_DIR."/lib/util.php");
 
 if (!$users->isLoggedIn())
 	$page->show403();
 
 if (!isset($_COOKIE['sabnzbd_'.$users->currentUserId().'__apikey']) || $_COOKIE['sabnzbd_'.$users->currentUserId().'__apikey'] == "")
 	$page->show404();
-	
-$page->smarty->assign('sabserver',$_COOKIE['sabnzbd_'.$users->currentUserId().'__host']);	
 
+$key = $_COOKIE['sabnzbd_'.$users->currentUserId().'__apikey'];
+$server = $_COOKIE['sabnzbd_'.$users->currentUserId().'__host'];	
+
+if (isset($_REQUEST["del"]))
+{
+	getUrl($server."api/?mode=queue&name=delete&value=".$_REQUEST["del"]."&apikey=".$key);
+}
+
+$page->smarty->assign('sabserver',$server);	
 $page->title = "Your Download Queue";
 $page->meta_title = "View Sabnzbd Queue";
 $page->meta_keywords = "view,sabznbd,queue";
--- www/pages/queuedata.php
+++ www/pages/queuedata.php
@@ -11,20 +11,27 @@ if (!isset($_COOKIE['sabnzbd_'.$users->currentUserId().'__apikey']))
 	
 $server = $_COOKIE['sabnzbd_'.$users->currentUserId().'__host'];
 $key = $_COOKIE['sabnzbd_'.$users->currentUserId().'__apikey'];
+$output = "";
 
 $json = getUrl($server."api/?mode=qstatus&output=json&apikey=".$key);
 if ($json !== false)
 {
 	$obj = json_decode($json);
-
 	$queue = $obj->{'jobs'};
-	
 	$count = 1;
 	
 	if (count($queue) > 0)
 	{
-		print "<table class=\"data\">";
-		print "<tr><th></th><th></th><th style='width:80px;text-align:right;'>size (mb)</th><th style='width:80px;text-align:right;'>left (mb)</th><th style='width:50px;text-align:right;'></th></tr>";
+		$output.="<table class=\"data\">";
+		$output.="<tr>
+		<th></th>
+		<th>Name</th>
+		<th style='width:80px;text-align:right;'>size (mb)</th>
+		<th style='width:80px;text-align:right;'>left (mb)</th>
+		<th style='width:50px;text-align:right;'>%</th>
+		<th style='text-align:right;'>time left</th>
+		<th></th>
+		</tr>";
 		foreach ($queue as $item)
 		{
 			if (strpos($item->{'filename'}, "fetch NZB") > 0)
@@ -32,32 +39,36 @@ if ($json !== false)
 			}
 			else
 			{
-				print "<tr>";
-				print "<td style='text-align:right;padding-right:10px;'>".$count.")</td>";
-				print "<td>".$item->{'filename'}."</td>";
-				print "<td style='text-align:right;'>".number_format(round($item->{'mb'}))."</td>";
+				$output.="<tr>";
+				$output.="<td style='text-align:right;padding-right:10px;'>".$count.")</td>";
+				$output.="<td>".$item->{'filename'}."</td>";
+				$output.="<td style='text-align:right;'>".number_format(round($item->{'mb'}))."</td>";
 				if ($count ==1)
 				{
-					print "<td class='right'>".number_format(round($item->{'mbleft'}))."</td>";
-					print "<td class='right'>".($item->{'mb'}==0?0:round($item->{'mbleft'}/$item->{'mb'}*100))."%</td>";
+					$output.="<td class='right'>".number_format(round($item->{'mbleft'}))."</td>";
+					$output.="<td class='right'>".($item->{'mb'}==0?0:round($item->{'mbleft'}/$item->{'mb'}*100))."%</td>";
 				}
 				else
 				{
-					print "<td></td><td></td>";
+					$output.="<td></td><td></td>";
 				}
-				print "</tr>";
+				$output.="<td style='text-align:right;'>".$item->{'timeleft'}."</td>";
+				$output.="<td style='text-align:right;'><a href='?del=".$item->{'id'}."'>delete</a></td>";
+				$output.="</tr>";
 				$count++;
 			}
 		}
-		print "</table>";
+		$output.="</table>";
 	}
 	else
 	{
-		print "<p>The queue is currently empty.</p>";
+		$output.="<p>The queue is currently empty.</p>";
 	}
 }
 else
 {
-	print "<p>Error retreiving queue.</p>";
+	$output.="<p>Error retreiving queue.</p>";
 }
+
+print $output;
 ?>
